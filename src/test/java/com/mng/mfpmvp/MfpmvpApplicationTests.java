dd7a1b3f703b9b28bdf9dc7b2b0d08c3b260a259
Mohan Gavande <mohangavande@outlook.com>
2025-01-15 17:57:40 +0100

    Updated Test files

--- src/test/java/com/mng/mfpmvp/MfpmvpApplicationTests.java
+++ src/test/java/com/mng/mfpmvp/MfpmvpApplicationTests.java
@@ -2,12 +2,27 @@ package com.mng.mfpmvp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class MfpmvpApplicationTests {
+	@Autowired
+	private DataSource dataSource;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testDatabaseConnection() throws SQLException {
+		try (Connection connection = dataSource.getConnection()) {
+			assertThat(connection).isNotNull();
+		}
 	}
 
 }

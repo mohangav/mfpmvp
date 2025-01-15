4fcf3ced2d72cddf221f64fdc8103a0f58ccb888
Mohan Gavande <mohangavande@outlook.com>
2025-01-15 16:50:53 +0100

    Initial commit

---
+++ src/main/java/com/mng/mfpmvp/MfpmvpApplication.java
@@ -0,0 +1,13 @@
+package com.mng.mfpmvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfpmvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfpmvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mng/mfpmvp/MfpmvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mng.mfpmvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfpmvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
